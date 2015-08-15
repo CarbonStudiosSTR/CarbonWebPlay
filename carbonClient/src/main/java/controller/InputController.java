ca2f7bbbdf1686d04d6fbafbbab63c531210e9b5
yatne <antonimaciej@gmail.com>
2015-08-15 15:28:08 +0200

    początek sprzątania (15%)

--- carbonClient/src/main/java/controller/InputController.java
+++ carbonClient/src/main/java/controller/InputController.java
@@ -2,16 +2,16 @@ package controller;
 
 
 import controller.controllers.OnKeyPressedController;
-import controller.controllers.OnKeyReleasedController;
-import controller.controllers.OnMousePressedController;
+import controller.controllers.onKeyReleasedController;
+import controller.controllers.onMusePressedController;
 import controller.controllers.OnMouseReleasedController;
 import javafx.scene.image.ImageView;
 
 public class InputController {
     public static void initInputController(ImageView imageView) {
         imageView.setOnKeyPressed(new OnKeyPressedController());
-        imageView.setOnKeyReleased(new OnKeyReleasedController());
-        imageView.setOnMousePressed(new OnMousePressedController());
+        imageView.setOnKeyReleased(new onKeyReleasedController());
+        imageView.setOnMousePressed(new onMusePressedController());
         imageView.setOnMouseReleased(new OnMouseReleasedController());
     }
 }
---
+++ carbonClient/src/main/java/controller/controllers/onKeyReleasedController.java
@@ -0,0 +1,10 @@
+package controller.controllers;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+
+public class onKeyReleasedController implements EventHandler<KeyEvent> {
+    @Override
+    public void handle(KeyEvent keyEvent) {
+    }
+}
