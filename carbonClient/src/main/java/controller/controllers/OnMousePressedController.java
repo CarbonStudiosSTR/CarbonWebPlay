3eeab3405a422aa332e5f9e07a66022bb773e33f
yatne <antonimaciej@gmail.com>
2015-08-15 15:38:30 +0200

    początek sprzątania (18%) (jebany git)

---
+++ carbonClient/src/main/java/controller/controllers/OnMousePressedController.java
@@ -0,0 +1,11 @@
+package controller.controllers;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
+public class OnMousePressedController implements EventHandler<MouseEvent> {
+    @Override
+    public void handle(MouseEvent mouseEvent) {
+
+    }
+}
